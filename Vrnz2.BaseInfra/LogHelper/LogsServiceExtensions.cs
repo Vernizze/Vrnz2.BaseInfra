d21b530e5a82e0b2dc327d7e5aed14ba1b43f712
Vernizze <cfvernizze@gmail.com>
2020-12-16 13:23:33 -0300

    Inclusao de Helper de Logs

---
+++ Vrnz2.BaseInfra/LogHelper/LogsServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace Vrnz2.BaseInfra.Logs
+{
+    public static class LogsServiceExtensions
+    {
+        public static ILogger Config(LogEventLevel minLogEventLevel = LogEventLevel.Information, string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+            => Log.Logger = new LoggerConfiguration()
+                   .MinimumLevel.Is(minLogEventLevel)
+                   .WriteTo.Console(minLogEventLevel, outputTemplate: outputTemplate, theme: AnsiConsoleTheme.Code)
+                   .CreateLogger();
+
+        public static IServiceCollection AddLogs(this IServiceCollection services, LogEventLevel minLogEventLevel = LogEventLevel.Information, string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+            => services.AddSingleton(_ => Config(minLogEventLevel, outputTemplate).ForContext<ILogger>());
+
+        public static IServiceCollection AddLogs(this IServiceCollection services, ILogger logger, LogEventLevel minLogEventLevel = LogEventLevel.Information, string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+            => services.AddSingleton(_ => logger.ForContext<ILogger>());
+    }
+}
