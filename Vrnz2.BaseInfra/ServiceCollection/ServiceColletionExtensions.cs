ecee0fcc2da66971cd8b3f0b389a86c293d74ba1
Vernizze <cfvernizze@gmail.com>
2020-12-16 13:21:21 -0300

    Inclusao de Extensao de IServiceCollection

---
+++ Vrnz2.BaseInfra/ServiceCollection/ServiceColletionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Vrnz2.BaseInfra.ServiceCollection
+{
+    public static class ServiceColletionExtensions
+    {
+        public static IServiceCollection AddIServiceColletion(this IServiceCollection services)
+            => services.AddSingleton(services);
+    }
+}
