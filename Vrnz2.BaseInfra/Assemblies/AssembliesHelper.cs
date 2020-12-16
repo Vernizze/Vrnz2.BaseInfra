31f4d6e5251c66385cac442b746586e841ca12bf
Vernizze <cfvernizze@gmail.com>
2020-12-16 13:19:25 -0300

    Inclusao de Helper de Assemblies

---
+++ Vrnz2.BaseInfra/Assemblies/AssembliesHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace Vrnz2.BaseInfra.Assemblies
+{
+    public static class AssembliesHelper
+    {
+        #region Methods
+
+        public static Assembly GetAssemblies() => typeof(AssembliesHelper).GetTypeInfo().Assembly;
+
+        public static Assembly GetAssemblies<T>() => typeof(T).GetTypeInfo().Assembly;
+
+        #endregion
+    }
+}
